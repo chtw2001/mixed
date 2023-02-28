a521e4ba62d94f755e6cd22badd1a90ef7712d2b
chtw2001 <jungtaekwon1019@gmail.com>
2023-03-01 00:24:35 +0900

    add verify url

--- blossom/settings.py
+++ blossom/settings.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
     
     'rest_framework',
     'rest_framework.authtoken',
+    'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist', # for Refreshed Tokens
     'corsheaders',
     'rest_auth',
